950606226b71ab7390193559bb3d8d6b32101917
Paju980 <himanshuwandhare13@gmail.com>
2024-08-14 23:32:35 +0530

    initial commit

---
+++ client/src/socket.ts
@@ -0,0 +1,22 @@
+import { io } from "socket.io-client";
+
+const URL = "http://localhost:3000";
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.log(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
+
+export const socket = io(URL);
